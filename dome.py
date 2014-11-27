3fc856b215fedfaff2744b173bcfa0a62a829955
Richard Busby <rsbusby@gmail.com>
2014-11-27 19:19:22 +0000

    thanksgiving 01

--- dome.py
+++ dome.py
@@ -10,49 +10,30 @@ from datetime import datetime
 import random
 from math import floor
 
-GPIO.setmode(GPIO.BCM)
-
-# 10 inputs
-pins = [11, 15 ,17,18,21,22,23,24,25]
-pins = [4, 17, 18]
-#GPIO.setup(11, GPIO.IN)
-for pin in pins:
-    GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
-#MP = 'aplay'
-MP = 'omxplayer' # --vol -1 lynx.wav --pos 18
 
 class touchPlay(object):
 
-    def __init__(self, pin, fileList, duration = None, name=None, timeout=20, wait=0, sustain=False):
-        if not name:
-            self.name = "signal" + str(pin)
-        else:
-            self.name = name
-        self.duration = duration
-        if duration and duration != 0:
-            self.dOpt = ' -d ' + str(duration)
-        else:
-            self.dOpt = ''
-        #try:
-        #    self.wavFile = fileList[0]
-        #except:
-        #    self.wavFile = None
+    def __init__(self, pin, fileList, duration = None, timeout=20, sustain=False, vol=0):
         self.fileList = fileList
         self.pin = pin
         self.timeout = timeout
         self.sustain = sustain
-        self.lastTime = datetime.now()
-        self.wait = wait
-        self.mp = MP
-        self.pos = 0
+        self.vol = vol
+        if self.vol:
+            self.volOpt = " --vol " + str(vol)
+        else:
+            self.volOpt = ''
         self.fileDict = {}
         for f in self.fileList:
             self.fileDict[f] = self.getLength(f)
         self.wavFile = self.getFile()
         print self.wavFile   
         self.setLength()
-        
+        self.iter = 0
+        self.pos = 0        
+        self.playing = False
+        self.startTime = None
+        self.lastTime = datetime.now()
 
     def getLength(self, soundFile):
         info = commands.getoutput("omxplayer " + str(soundFile) + " --info")
@@ -79,27 +60,9 @@ class touchPlay(object):
             return None
         if len(self.fileList) == 1:
             return self.fileList[0]
-        #choice = random.randint(0,len(self.fileList)-1)
         choice = random.choice(list(self.fileDict.keys()))
         print "Picking " + choice
         return choice
-
-    def setDuration(self, d):
-        self.duration = d
-        if d and d != 0:
-            self.dOpt = ' -d ' + str(d)
-        else:
-            self.dOpt = ''
-
-    def setPos(self, sec):
-        print "sec " + str(sec) + "  len: " + str(self.length)
-        if sec > self.length:
-            self.getFile()
-            self.pos = 0
-            print "set to zero"
-        else:
-            self.pos = sec
-            print "set to " + str(sec)
 
     def appendPos(self, delta):
         sec = float(str(delta).split(':')[-1].lstrip('0'))
@@ -107,152 +70,127 @@ class touchPlay(object):
         #print str(self.pos) + "  "  + str(self.length)
         if self.pos > self.length:
             self.pos = 0
-        
             
     def killSound(self):
         ''' stop playing '''        
         psOut = commands.getoutput('ps -ax | grep ' + self.wavFile )
-        print "killing for pin " + str(self.pin )
-        print psOut
+        #print str(pin) + " killing "
+        #print psOut
         lines = psOut.split('\n')
         for line in lines:
             pid = line.split()[0].strip()
             if 'aplay' in line or 'omxplay' in line:
-                print pid
+                #print pid
                 commands.getoutput('kill -9 ' + pid )
+        self.playing = False
+        self.startTime = None
+
+        if self.length - self.pos  < float(self.length) / 4.0:
+            self.pos = 0
     
     def check(self):
-        #print str(self.pin)  + "  " + str(GPIO.input(self.pin) ) 
 
-        playing = None
+        #print str(self.pin) + " " + str(GPIO.input(self.pin))   + "  pos: " + str(self.pos)  + " " + str(self.iter)
+        #self.iter += 1
 
-        print str(self.pin) + " " + str(GPIO.input(self.pin))   + "  pos: " + str(self.pos)  
-
-        if self.wavFile:
-            playing = int(commands.getoutput('ps -aux | grep ' +self.mp +' | grep ' + self.wavFile + ' | wc -l')) > 1
-            
+        senseVal = GPIO.input(self.pin)
      	# if signal present
-        if (GPIO.input(self.pin) == 0 ):
-            print "on"
-            if not playing:
+        now = datetime.now()
+        if self.playing:
+            delta = now - self.startTime
+            if delta.total_seconds() > self.length:
+                self.playing = False
+                self.pos = 0
+                self.startTime = None
+
+
+        if senseVal == 0:
+            #print "on"
+            if not self.playing:
                 if self.pos <= 0 or self.pos >= self.length:
                     self.wavFile = self.getFile()
                     self.setLength()
                 if self.wavFile:
-                    if self.mp == 'aplay':
-                        os.system("aplay " + self.wavFile + self.dOpt + " &")
+                    if self.pos <= 0 and not self.sustain:
+                        os.system("aplay " + self.wavFile + " &")
+                        print "starting " + self.wavFile + " " #+ str(self.iter)
                     else:
                         posOpt = ''
                         if self.pos:
                             posOpt = " --pos " + str(self.pos)
-                        os.system("omxplayer " + self.wavFile + " " + self.dOpt + posOpt + " &")
-                        print "starting " + self.wavFile
+                        os.system("omxplayer --no-osd " + self.wavFile + " " + posOpt + self.volOpt + " &")
+                        print "starting " + self.wavFile + " with omx " #+ str(self.iter)
+                        # adjust pos because omxplayer takes a while to start
                         self.pos = self.pos - 1.5
-                        self.lastTime = datetime.now()
-                    #print self.name 
-                #print self.duration
-            #print self.name
-
-            # keep sustaining
-            if playing and self.sustain:
-                now = datetime.now()
-                delta = now - self.lastTime
-                #print "delta " + str(delta)
-                self.appendPos(delta)
-                self.lastTime = datetime.now()
-                #print self.pos
-
-                #print "Sustaining " + self.name
-        elif GPIO.input(self.pin) == True:
-            
-	        # If sustain and no recent trigger, then stop sound.
-            if self.sustain and playing:
-                now = datetime.now()
-                delta = now - self.lastTime
-                if delta.total_seconds() > self.timeout:
-                    self.killSound()
-                    self.appendPos(delta)
+                    #now = datetime.now()
                     self.lastTime = now
-                    print "kiilled"
-                    print self.pos
-        
-        else:
-            # do nothing
-            p = 8
+                    self.playing = True
+                    self.startTime = now
+
+            if self.playing and self.sustain:
+                self.lastTime = now
+
+        else: # senseVal == 1:
+            # If sustaining, but no recent trigger, then stop sound.
+            if self.sustain and self.playing:
+                #now = datetime.now()
+                delta = now - self.lastTime
+                #print str(self.pin) + " " + str(self.timeout - delta.total_seconds())
+                if delta.total_seconds() > self.timeout:
+                    self.appendPos(delta)
+                    self.killSound()
+                    print str(self.pin) + " killed at " + str(self.pos)
+                    self.lastTime = now
+                    
+                
 
 
-
-#fileList = [
-
-#'/home/pi/sounds/moose.wav',
-#'/home/pi/sounds/lynx.wav',
-#'/home/pi/sounds/windy_birds.wav',
-#'/home/pi/sounds/windy_birds.wav',
-#'/home/pi/sounds/windy_birds.wav',
-    
-
-#]
-import os
 sdir = '/home/pi/domeSensor'
+#sdir = '/media/TEST/sounds'
+
 def filesFromDir(d, wd=None):
     if not wd:
         wd = sdir
     #print wd
     return [wd + '/' + d+'/'+ f for f in os.listdir(wd + '/' + d)]
 
-#print os.listdir('t1')
 
-#sys.exit()    
+GPIO.setmode(GPIO.BCM)
+
+# 10 inputs
+pins = [4, 11, 15 ,17,18,21,22,23,24]
+#pins = [4, 17, 18]
+for pin in pins:
+    GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
 touchSensors = []
-p1 = touchPlay(4, filesFromDir('p1'), timeout=6, sustain=True)
+
+p1 = touchPlay(11, filesFromDir('p1'), timeout=20, sustain=True)
 touchSensors.append(p1)
-p2 = touchPlay(17, filesFromDir('p2'), timeout=6, sustain=True)
+p2 = touchPlay(15, filesFromDir('p2'), timeout=10, sustain=True, vol=0)
 touchSensors.append(p2)
-p3 = touchPlay(18, filesFromDir('p2'), timeout=45, sustain=True)
+p3 = touchPlay(17, filesFromDir('p3'), timeout=15, sustain=True)
 touchSensors.append(p3)
-#t11 = touchPlay(11, filesFromDir('p1'), timeout=1, sustain=True)
-#t14 = touchPlay(14, filesFromDir('p2'), timeout=1, sustain=True)
-#t15 = touchPlay(15, filesFromDir('p3'), timeout=1, sustain=True)
-#t17 = touchPlay(17, filesFromDir('p4'), timeout=1, sustain=True)
-#t18 = touchPlay(18, filesFromDir('p5'), timeout=1, sustain=True)
+p4 = touchPlay(18, filesFromDir('p4'), timeout=15, sustain=True, vol=0)
+touchSensors.append(p4)
+p5 = touchPlay(21, filesFromDir('p5'), timeout=20, sustain=True)
+touchSensors.append(p5)
 
-#t21 = touchPlay(21, filesFromDir('t1'), 8)
-#t22 = touchPlay(22, ['/home/pi/sounds/lynxno.wav'], 50)
-#t23 = touchPlay(23, ['/home/pi/sounds/moose.wav'], 6)
-#t24 = touchPlay(24, ['/home/pi/sounds/monkeys.wav'], 5)
-#t25 = touchPlay(25, ['/home/pi/sounds/moose.wav'], 60)
+t1 = touchPlay(22, filesFromDir('t1'), timeout=5, sustain=False)
+touchSensors.append(t1)
+t2 = touchPlay(23, filesFromDir('t2'), timeout=5, sustain=False)
+touchSensors.append(t2)
+t3 = touchPlay(24, filesFromDir('t3'), timeout=10, sustain=False)
+touchSensors.append(t3)
+t4 = touchPlay(4, filesFromDir('t4'), timeout=5, sustain=False)
+touchSensors.append(t4)
 
 
+# main loop
 while True:
 
     for s in touchSensors:
         s.check()
 
     #sleep(0.15)
-
-    #t11.check()
-    #t14.check()
-    #t15.check()
-    #t17.check()
-    #t18.check()
-
-    #t21.check()
-    #t22.check()
-    #t23.check()
-    #t24.check()
-    #t25.check()
-
-
-    # change duration randomly each time
-    #t11.setDuration(random.randint(2, 12))
-    #t14.setDuration(random.randint(2, 12))
-    #t15.setDuration(random.randint(2, 12))
-    #t17.setDuration(random.randint(2, 12))
-    #t18.setDuration(random.randint(2, 12))
-
-    #t21.setDuration(random.randint(2, 12))
-    #t22.setDuration(random.randint(4, 14))
-    #t23.setDuration(random.randint(4, 14))
-    #t24.setDuration(random.randint(5, 12))
     #t25.setDuration(random.randint(2, 14))        
-        
